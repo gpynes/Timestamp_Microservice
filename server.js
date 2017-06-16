d4b6239105902e992e6df2c71e0b19abbdcdce59
gpynes <gpynes@gmail.com>
2017-06-16 12:33:12 +0200

    Updates to more up to date version of ecmascript

--- server.js
+++ server.js
@@ -1,36 +1,28 @@
-// server.js
-// where your node app starts
-
 // init project
-var express = require('express');
-var app = express();
-
-// we've started you off with Express,
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+const express = require('express')
+const app = express()
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+app.get('/', (request, response) => {
+  response.sendFile(`${__dirname}/views/index.html`)
+})
 
 // timestamp query parser
-app.get('/:timestamp', function(request, response) {
-  var query = request.params.timestamp;
+app.get('/:timestamp', (request, response) => {
+  const query = request.params.timestamp
 
-  var date = new Date(+query * 1000 || query);
+  const date = new Date(+query * 1000 || query)
 
-  var dateObj = {
+  const dateObj = {
     unix: date.getTime() / 1000,
     natural: Date.parse(date) ? date.toLocaleString('en-us', { year: 'numeric', month: 'long', day: 'numeric'}) : null
-  };
-  response.json(dateObj);
-});
+  }
+  response.json(dateObj)
+})
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+const listener = app.listen(process.env.PORT, () => console.log(`Your app is listening on port ${listener.address().port}`) )
